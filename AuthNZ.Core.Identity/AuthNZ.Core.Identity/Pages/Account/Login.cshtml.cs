6a5d9d7aa97950dcab68539cccdec0fbaac7212e
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-08-11 16:26:01 +0530

    Basic Skeleton Added

---
+++ AuthNZ.Core.Identity/AuthNZ.Core.Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthNZ.Core.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
