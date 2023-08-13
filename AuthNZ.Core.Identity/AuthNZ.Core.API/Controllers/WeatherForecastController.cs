8d7abaf3ac875ca0bab6aef56a17726152bafd50
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-08-13 21:07:50 +0530

    corrected the default weather controller

--- AuthNZ.Core.Identity/AuthNZ.Core.API/Controllers/WeatherForecastController.cs
+++ AuthNZ.Core.Identity/AuthNZ.Core.API/Controllers/WeatherForecastController.cs
@@ -6,28 +6,42 @@ namespace AuthNZ.Core.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly (int Min, int Max, string Summary)[] TemperatureSummaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            (-20, -1, "Freezing"),
+            (0, 9, "Bracing"),
+            (10, 19, "Chilly"),
+            (20, 29, "Cool"),
+            (30, 39, "Mild"),
+            (40, 49, "Warm"),
+            (50, 55, "Scorching")
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Random _rng = new Random();
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            return Enumerable.Range(1, 5).Select(index => {
+                var temperatureC = _rng.Next(-20, 56);
+                var summary = TemperatureSummaries.First(s => temperatureC >= s.Min && temperatureC <= s.Max).Summary;
+
+                return new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = temperatureC,
+                    Summary = summary
+                };
             })
             .ToArray();
+        }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+            public int TemperatureC { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+            public string Summary { get; set; }
         }
     }
 }
